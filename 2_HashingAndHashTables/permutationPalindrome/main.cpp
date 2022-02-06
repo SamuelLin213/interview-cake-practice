6a24d707dc4b74edf2462e0cec7ea6297aa052d4
Samuel <samuelddlin@gmail.com>
2022-02-06 07:38:47 +0000

    finish second solution

--- 2_HashingAndHashTables/permutationPalindrome/main.cpp
+++ 2_HashingAndHashTables/permutationPalindrome/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <unordered_set>
 #include <unordered_map>
 using namespace std;
 
@@ -13,6 +14,7 @@ void print_map(unordered_map<char, int> const &m)
 int main()
 {
   unordered_map<char, int> palindrome;
+  unordered_set<char> counts;
   string str;
   char middle;
   bool palin = true;
@@ -21,36 +23,51 @@ int main()
   cout << "Enter a string: ";
   cin >> str;
 
+  //First solution: Brute Force
+  // for(int x = 0; x < str.length(); x++)
+  // {
+  //   auto it = palindrome.find(str[x]);
+  //   if(it != palindrome.end())
+  //   {
+  //     palindrome[str[x]] += 1;
+  //   }
+  //   else{
+  //     palindrome.insert(make_pair(str[x], 1));
+  //   }
+  // }
+  //
+  // print_map(palindrome);
+  //
+  // // unordered_map<int, int>::iterator it;
+  // //it = palindrome.begin(); it != palindrome.end(); it++
+  // for(auto it: palindrome)
+  // {
+  //     if(it.second % 2 != 0)
+  //     {
+  //       odd++;
+  //     }
+  //     if(odd > 1)
+  //     {
+  //       palin = false;
+  //       break;
+  //     }
+  // }
+  //
+  // cout << "The string " << str << " is" << ((palin) ? " " : " not ") << "a palindrome." << endl;
+
+  //Second solution: Using set
   for(int x = 0; x < str.length(); x++)
   {
-    auto it = palindrome.find(str[x]);
-    if(it != palindrome.end())
+    if(counts.find(str[x]) == counts.end())
     {
-      palindrome[str[x]] += 1;
+        counts.insert(str[x]);
     }
     else{
-      palindrome.insert(make_pair(str[x], 1));
+      counts.erase(counts.find(str[x]));
     }
   }
 
-  print_map(palindrome);
-
-  // unordered_map<int, int>::iterator it;
-  //it = palindrome.begin(); it != palindrome.end(); it++
-  for(auto it: palindrome)
-  {
-      if(it.second % 2 != 0)
-      {
-        odd++;
-      }
-      if(odd > 1)
-      {
-        palin = false;
-        break;
-      }
-  }
-
-  cout << "The string " << str << " is" << ((palin) ? " " : " not ") << "a palindrome." << endl;
+  cout << "The string " << str << " is" << ((counts.size() <= 1) ? " " : " not ") << "a palindrome." << endl;
 
   return 0;
 }
